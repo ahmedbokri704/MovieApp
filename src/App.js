133ab9a74785c6418d2ab5cef8435edb1195d042
ahmedbokri <ahmedbokri25@gmail.com>
2020-04-16 14:22:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import './App.css';
-import MoviePage from './pages/MoviePage'
+import NavBar from './navbar'
 
 function App() {
+  const nav = [
+    { title: 'home', lien: '#home', },
+    {
+      title: 'service', lien: '#service', subelement: [
+        { title: 'For entrepeneurs', lien: "#entrepeneurs" },
+        { title: 'For students', lien: '#students' },
+        { title: 'For hobbysties', lien: '#hobbysties' }]
+    },
+    { title: 'contact', lien: '#contact' }
+  ]
   return (
     <div className="App">
-        <MoviePage />
+      <div class="container center">
+        <NavBar nav={nav} />
+      </div>
     </div>
   );
 }
-
 export default App;
